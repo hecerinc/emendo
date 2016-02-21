49781eaf85cb34d22812a1c87faeb8375f743c83
Irvel <irvel.nduva@gmail.com>
2016-02-20 18:42:40 -0600

    Implement issue vote logic

--- src/Controller/VotesController.php
+++ src/Controller/VotesController.php
@@ -51,14 +51,46 @@ class VotesController extends AppController
      */
     public function add()
     {
-        $vote = $this->Votes->newEntity();
-        if ($this->request->is('post')) {
-            $vote = $this->Votes->patchEntity($vote, $this->request->data);
-            if ($this->Votes->save($vote)) {
-                $this->Flash->success(__('The vote has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The vote could not be saved. Please, try again.'));
+        // Always false to allow testing
+        if(!$this->request->is('ajax') && false)
+            throw new NotFoundException('You can\'t access that');
+        // Extract variables from data
+        extract($this->request->data);
+        // If the vote was made to an issue
+        if($issue_id != NULL){
+            // Check if vote already exists
+            $req_vote = $this->Votes->find('all', [
+                'conditions'=>['issue_id'=>$issue_id, 'user_id'=>$user_id]
+                ]);
+            echo ("<pre>");
+            var_dump($req_vote->toArray());
+            //var_dump($req_vote->isEmpty());
+            echo ("</pre>");
+            if($req_vote->isEmpty()){ 
+                // No vote exists yet, then create vote
+                $newVote = $this->Votes->newEntity();
+                // Uncomment this later
+                // $this->request->data['user_id'] = $this->Auth->user('id');
+                $this->request->data['user_id'] == 1;
+                $newVote = $this->Votes->patchEntity($newVote, 
+                                                    $this->request->data);
+                // try to save
+                echo ("<pre>");
+                var_dump($newVote);
+                echo ("</pre>");
+                if($this->Votes->save($newVote)){
+                    echo("aksjdfaoisdjfoiasdjfio");
+                    // Vote was successfull
+                    // Update vote count
+                    updateCount($issue_id, true);
+                }
+            }
+            else{
+                // There are 3 operations for a vote update:
+                // Change vote from positive to negative
+                // Change vote form negative to positive
+                // Delete vote (When both are positive or both are negative)
+                //if($req_vote[]
             }
         }
         $users = $this->Votes->Users->find('list', ['limit' => 200]);
